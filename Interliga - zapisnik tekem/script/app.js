84668df1954289a8a35d7581109543540663f075
Vid Kranjec <84270214+Kranjex@users.noreply.github.com>
2021-07-25 18:15:44 +0200

    Delete Interliga - zapisnik tekem directory

--- Interliga - zapisnik tekem/script/app.js
+++
@@ -1,9 +0,0 @@
-const windowWidth = window.innerWidth;
-const windowHeight = window.innerHeight;
-const ratio = window.devicePixelRatio;
-
-const screenWidth = screen.width;
-const screenHeight = screen.height;
-
-console.log(screenWidth, screenHeight);
-console.log(windowWidth, windowHeight, ratio);
