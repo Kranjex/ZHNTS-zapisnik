9a51f958b7d2dfa15b7f74504f239aecb93e0097
Vid Kranjec <84270214+Kranjex@users.noreply.github.com>
2021-09-02 22:58:08 +0200

    Delete Interliga - zapisnik tekem/public directory

--- Interliga - zapisnik tekem/public/javascript/matchReportForm.js
+++
@@ -1,6 +0,0 @@
-const printButton = document.getElementById('printButton');
-const editButton = document.getElementById('editButton');
-
-printButton.addEventListener('click', () => {
-  window.print();
-});
